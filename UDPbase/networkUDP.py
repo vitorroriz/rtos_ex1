0c7aabafe85034473bd9e18b88c2662204716508
Vitor Roriz <Vitor@Vitors-MacBook-Pro.local>
2017-02-20 20:55:57 +0100

    Correcting getmyip method

--- UDPbase/networkUDP.py
+++ UDPbase/networkUDP.py
@@ -5,9 +5,6 @@ from collections import namedtuple
 import threading
 
 
-
-
-
 class networkUDP:
     'Class that defines a simple UDP server'
     def __init__(self, serverport, serverhost = None):
@@ -21,84 +18,90 @@ class networkUDP:
         self.shutdown   =  False
         self.MAX_PKT_SIZE = 512
 
-    def makeserversocket(self):
+    def __makeserversocket(self, addr = None ):
         #Creating socket (UDP)
+        if addr == None:
+            addr_to_bind = self.serveraddr
+        else:
+            addr_to_bind = addr
+
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP socket
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1) #Let port be reused when socket is closed
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #enable broadcast
+
         #Bind socket to a port
         print 'Starting up on %s port %d' % self.serveraddr
-        sock.bind(self.serveraddr)
+        sock.bind(addr_to_bind)
         return sock
 
     def listen(self):
-        sock = self.makeserversocket()
+        sock = self.__makeserversocket()
 
         while not self.shutdown:
             try:
-                #print '\nWaiting to receive message'
                 data, addr = sock.recvfrom(self.MAX_PKT_SIZE)
 
                 print 'received %s bytes from %s' % (len(data), addr)
                 print data
 
-#                if data:
-#                   sent = sock.sendto(data,addr)
-#           print 'sent %s bytes back to %s' % (sent,addr)
             except KeyboardInterrupt:
                 self.shutdown = True
         sock.close()
         print '\nServer closed by Keyboard interruption'
 
     
-    def sendto(self, addr, message = 'This is the client default message :D!'):
+    def sendto(self, addr , message = 'This is the client default message :D!'):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1) #Let port be reused when socket is closed
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #enable broadcast
                   
-
         try:
-    #       print 'Sending "%s"' % message
             sent = sock.sendto(message, addr)
-    #       print 'Waiting to receive'
-    #        data, server = sock.recvfrom(512)
-    #           print 'received "%s" from %s' % (data,server)
-
         finally:
-#           print 'closing client socket'
             sock.close()
 
     def broadcast(self, message):
-        self.sendto(('255.255.255.255',self.serverport), message)
+        self.sendto(('192.168.1.255',self.serverport), message)
                     
 
     def getmyip(self):
             dummysocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            dummysocket.connect(('google.com',0))
+            dummysocket.connect(('8.8.8.8',80)) #connecting to google to find my IP
             return dummysocket.getsockname()[0]
 
 
-net1 = networkUDP(20000)
-
-
 def serverhand():
+    net1 = networkUDP(20001) 
+    print 'My IP is: ' + net1.getmyip()
     net1.listen()
-    
 def clienthand():
+    net2 = networkUDP(20000, serverhost = '192.168.1.37')
     while True:
         try:
             message = raw_input('-> ')
-            net1.sendto(('192.168.1.103'),30000)
+            #net1.sendto(('192.168.1.38'),30000)
+            net2.sendto(('192.168.1.37',20000), message)
         except KeyboardInterrupt:
             print 'Keyboard int'            
             sys.exit()
 
 
-
-
 def main():
-    net1 = networkUDP(30000)
     print 'Oi'
-    print net1.getmyip()
-    net1.broadcast('Message bcast')
+    #print net1.getmyip()
+    #net1 = networkUDP(20000, serverhost = '192.168.1.37')
+
+    ts = threading.Thread(target = serverhand)
+    tc = threading.Thread(target = clienthand)
+
+    ts.start()
+    tc.start()
+
+#   ts.join()
+    while tc.isAlive():
+        tc.join(5.0)
+
+
 
     
 if __name__ == '__main__':
