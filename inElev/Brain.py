4a76da54e00c07bcd22f31ffa9452d0c21419ad8
smyrberg <sondre.myrberg@gmail.com>
2017-03-07 20:42:02 +0100

    update

--- inElev/Brain.py
+++ inElev/Brain.py
@@ -49,9 +49,6 @@ class Brain(object):
 	def external_next_destin(self, elevator_IP):
 		#Returns the next_destin for a given elevator, based in the external requests
 
-		#translation for the dictionaries
-#		translation_d = {1 : "df2" , 2 : "df3" , 3 : "df4"}
-#		translation_u = {0 : "uf1" , 1 : "uf2" , 2 : "uf3"}
 		i_dic_ext = {"uf1" : 0 , "uf2" : 1, "uf3" : 2, "df2" : 1, "df3" : 2, "df4" : 3}
 		
 		distances = {}
@@ -78,32 +75,8 @@ class Brain(object):
 		for elevator in self.hierarchy.keys():
 		#	print "ex_destin of " + elevator + " is " + str(self.control_info[elevator]["ex_destin"])
 			if self.control_info[elevator]["ex_destin"] == i_dic_ext[button]:
-		#		print "BRAIN: returning 0 for button " + button + " elevator: " + elevator_IP + " because of " + elevator 			
+		#		print "BRAIN: returning 0 for button " + button + " elevator: " + elevator_IP + " because of " + 			
 				return 0
 	
 		return 1
 			
-
-
-
-	# def _internal_dir(self,current_floor, internal_request):
-	# 	if (internal_request - current_floor) < 0: return -1
-	# 	else: return 1 
-
-	# def set_destination(self):
-	# i_dic_int = {"cf1" : 0, "cf2" : 1 , "cf3" : 2 , "cf4" : 3}
-	# i_dic_ext = {"uf1" : 0 , "uf2" : 1, "uf3" : 2, "df2" : 1, "df3" : 2, "df4" : 3}
-	# dic_dir_ext = {"uf1" : 1, "uf2" : 1 , "uf3" : 1, "df2" : -1, "df3" : -1, "df4" : -1}
-
-
-
-	# internal_distances = {}
-
-	# 	for i in self.internals:
-	# 		if self.internals[i]:
-	# 			internal_dir_req = self._internal_dir(system_info_v["lastF"], i_dic_int[i])
-	# 			current_dir = self.system_info_v["lastDir"]
-	# 			if ((internal_dir_req == current_dir) || current_dir == 0):
-	# 				internal_distances[i] = i_dic_int[i] -  system_info_v["lastF"]
-		
-# 	
