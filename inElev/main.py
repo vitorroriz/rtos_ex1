54cf5c964918b73ea1b9bc4d021af5ee110340ee
Einarama <henriksen.einar@gmail.com>
2017-03-13 07:38:51 +0100

    update

--- inElev/main.py
+++ inElev/main.py
@@ -5,7 +5,7 @@ import datetime
 from inElev3 import Elevator
 
 def main():
-	elevator1 = Elevator(serverport = 51012, elevatorsList = {"129.241.187.38", "129.241.187.151", "129.241.187.146"})
+	elevator1 = Elevator(serverport = 51012, elevatorsList = {"129.241.187.38", "129.241.187.48", "129.241.187.46"})
 
 #	elevator1 = Elevator(serverport = 51012, hierarchy = {"129.241.187.48"})
 	ip = elevator1.myIP
